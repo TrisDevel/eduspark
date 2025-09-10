e9fff395a5c870e978e8b54a3038d1f7af799e55
vankit123 <kietvkse182744@fpt.edu.vn>
2025-09-10 13:30:17 +0700

    pull

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -131,7 +131,7 @@ export default function RegisterPage() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-eye-fill"
+                      className="bi bi-eye-fill"
                       viewBox="0 0 16 16"
                     >
                       <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
@@ -143,7 +143,7 @@ export default function RegisterPage() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-eye-slash-fill"
+                      className="bi bi-eye-slash-fill"
                       viewBox="0 0 16 16"
                     >
                       <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z" />
@@ -187,7 +187,7 @@ export default function RegisterPage() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-eye-fill"
+                      className="bi bi-eye-fill"
                       viewBox="0 0 16 16"
                     >
                       <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
@@ -199,7 +199,7 @@ export default function RegisterPage() {
                       width="16"
                       height="16"
                       fill="currentColor"
-                      class="bi bi-eye-slash-fill"
+                      className="bi bi-eye-slash-fill"
                       viewBox="0 0 16 16"
                     >
                       <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z" />
